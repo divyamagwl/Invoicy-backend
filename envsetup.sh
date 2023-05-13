47c6f20126f3744f6facae5e531e6113e7e754ff
Divyam <agrawaldivyam2001@gmail.com>
2023-05-13 17:55:34 +0530

    Updated env setup

--- envsetup.sh
+++ envsetup.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
+ls
+
+if [ -d "env" ] 
+then
+    echo "Python virtual environment exists."
+    rm -r env
+fi
+
+if [ -d "venv" ] 
+then
+    echo "Python virtual environment exists."
+    rm -r venv
+fi
 
 ls
+
 python3 -m venv venv
 
 source venv/bin/activate
