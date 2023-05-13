234dfc2e491ae2f5406e83acc9c27a76cb076e66
Divyam <agrawaldivyam2001@gmail.com>
2023-05-13 18:00:11 +0530

    Updated env setup

--- envsetup.sh
+++ envsetup.sh
@@ -1,28 +1,18 @@
 #!/bin/sh
-ls
-
-if [ -d "env" ] 
-then
-    echo "Python virtual environment exists."
-    rm -r env
-fi
-
 if [ -d "venv" ] 
 then
-    echo "Python virtual environment exists."
     rm -r venv
 fi
-
-ls
 
 python3 -m venv venv
 
 source venv/bin/activate
 
-ls 
+pip3 install -r backend/requirements.txt
+
 pip3 -V
 
-pip3 install -r backend/requirements.txt
+cat backend/users/tests.py
 
 python3 backend/manage.py test
 
